4ad5f2bb6d23943d66c70fda4036ce7c924a1d68
Sasha Sasha <sashasashaolh@gmail.com>
2023-10-26 17:38:37 +0300

    Improve user interface

--- src/alex_olhovskiy/Calculator/View.java
+++ src/alex_olhovskiy/Calculator/View.java
@@ -2,7 +2,10 @@ package alex_olhovskiy.Calculator;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -13,82 +16,73 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class View {
-	private JFrame window;
 	private Presenter presenter;
-	private String str;
+	private JFrame window;
+	private JTextField textField;
+	private int width=360;
+	private int height=400;
+	private int vGape=10;
+	private int hGape=10;
+	private int rows=4;
+	private int columns=5;
+	private int bSize=(width-(vGape*(columns-1)))/columns;
 	private StringBuilder sb;
+	private String[] buttonNames={"1","2","3","+","C","4","5","6","-","x^y","7","8","9","/","cos","0",".","=","*","sin"};
 	public View() {
-		sb=new StringBuilder();
 		presenter=new Presenter();
+		sb=new StringBuilder();
 		window=new JFrame();
-		window.setTitle("FlowLayout");
+		window.setTitle("GridLayout");
 		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		window.setSize(500,400);
-		window.setLocationRelativeTo(null);
+		window.setSize(width,height);
+		window.setLocationRelativeTo(null);//in center of the screen
+		window.setResizable(false);
+		textField=new JTextField(16);
+		textField.setFont(new Font("",Font.BOLD,24));
+
+		JPanel upperPanel=new JPanel();
+		JPanel downPanel=new JPanel();
 		
-		JPanel panel=new JPanel();
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
-		panel.setBackground(Color.RED);
-		window.add(panel,BorderLayout.CENTER);
+		upperPanel.add(textField,BorderLayout.CENTER);
+		downPanel.setLayout(new GridLayout(4,5,10,10));
 		
-		JTextField tf=new JTextField(20);
-		
-		
-		JButton button=new JButton("1");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Button '1' pressed");
-				sb.append("1");
-				tf.setText(sb.toString());
+		for(int i=0;i<20;i++)
+		{
+			JButton button=new JButton(buttonNames[i]);
+			button.setPreferredSize(new Dimension(bSize,bSize));
+			//button.setActionCommand(""+i);
+			if(i==4){
+				button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						sb.delete(0,sb.length());
+						textField.setText(sb.toString());
+					}
+				});
 			}
-		});
-		panel.add(button);
-		
-		JButton button2=new JButton("2");
-		button2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Button '2' pressed");
-				sb.append("2");
-				tf.setText(sb.toString());
+			else{
+				if(i==17){
+					button.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							System.out.println("Button '=' pressed");
+							textField.setText(presenter.getString(sb.toString()));
+						}
+					});
+				}
+				else {
+					button.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							System.out.println("Button "+button.getActionCommand()+" pressed");
+							sb.append(button.getActionCommand());
+							textField.setText(sb.toString());
+						}
+					});
+				}
 			}
-		});
-		panel.add(button2);
+			downPanel.add(button);
+		}
 		
-		JButton button3=new JButton("3");
-		button3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Button '3' pressed");
-				sb.append("3");
-				tf.setText(sb.toString());
-			}
-		});
-		panel.add(button3);
-		
-		JButton button4=new JButton("+");
-		button4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Button '+' pressed");
-				sb.append("+");
-				tf.setText(sb.toString());
-			}
-		});
-		panel.add(button4);
-		
-		JButton button5=new JButton("=");
-		button5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Button '=' pressed");
-				tf.setText(presenter.getString(sb.toString()));
-			}
-		});
-		panel.add(button5);
-		
-		
-		
-	
-		panel.add(tf);
-		
-		//panel.setPreferredSize(new Dimension(250,250));
+		window.add(upperPanel,BorderLayout.NORTH);
+		window.add(downPanel,BorderLayout.SOUTH);
 		
 		window.setVisible(true);
 	}
