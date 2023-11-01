a900d13a83785d2605e0a47e539f1e3fd36e6cfa
Sasha Sasha <sashasashaolh@gmail.com>
2023-11-01 23:19:49 +0300

    add model function

--- src/alex_olhovskiy/Calculator/Model.java
+++ src/alex_olhovskiy/Calculator/Model.java
@@ -4,95 +4,81 @@ import java.util.ArrayList;
 
 public class Model {
 	public String getString(String str) {
-		System.out.println("String from model "+str);
-		return "Answer String from model";
+		//System.out.println("String from model "+str);
+		return Double.toString(findChar(str));//"Answer String from model";
 	}
 	
-//	public static char findChar(String str) {
-//		char[]arr=str.toCharArray();
-//		char[]firstNum;
-//		for(int i=0;i<arr.length;i++)
-//		{
-//			if(((int)arr[i]>=48)&&((int)arr[i]<58))
-//			{
-//				
-//			}
-//		}
-//		return '!';
-//	}
-	
-//	public <T> getNum(ArrayList<Character> list){
-//		boolean isDouble=false;
-//		for(Character c:list)
-//		{
-//			if(c=='.')
-//			{
-//				isDouble=true;
-//			}
-//		}
-//		if(isDouble)
-//		{
-//			char[]arr=list.toArray();
-//		}
-//		return num;
-//	}
-//	
-//	public static char findChar(String str) {
-//		char[]arr=str.toCharArray();
-//		ArrayList<Character>firstNumList=new ArrayList<>();
-//		ArrayList<Character>secondNumList=new ArrayList<>();
-//		int action=0;
-//		boolean first=true;
-//		for(int i=0;i<arr.length;i++)
-//		{
-//			if((((int)arr[i]>=48)&&((int)arr[i]<=58))||((int)arr[i]==46))
-//			{
-//				if(first)
-//				{
-//					firstNum.add(arr[i]);
-//				}
-//				else
-//				{
-//					secondNum.add(arr[i]);
-//				}
-//			}
-//			else
-//			{
-//				first=false;
-//				action=(int)arr[i];
-//			}
-//		}
-//		
-//				switch(action)
-//				{
-//					case 42://*
-//						
-//						break;
-//					case 43://+
-//						break;
-//					case 45://-
-//						break;
-//					case 46://.
-//						break;
-//					case 47:///
-//						break;
-//					case 99://c
-//						break;
-//					case 97://a
-//						break;
-//					case 115://s
-//						break;
-//					case 98://b
-//						break;
-//					case 116://t
-//						break;
-//					case 100://d
-//						break;
-//					case 94://^
-//						break;
-//				}
-//			
-//		
-//		return '!';
-//	}
+	public double findChar(String str) {
+		char[]arr=str.toCharArray();
+		ArrayList<String>firstNumList=new ArrayList<>();
+		ArrayList<String>secondNumList=new ArrayList<>();
+		int action=0;
+		boolean first=true;
+		boolean firstNumDouble;
+		boolean secondNumDouble;
+		for(int i=0;i<arr.length;i++)
+		{
+			if((((int)arr[i]>=48)&&((int)arr[i]<=58))||((int)arr[i]==46))
+			{
+				if(first)
+				{
+					firstNumList.add(""+arr[i]);
+				}
+				else
+				{
+					secondNumList.add(""+arr[i]);
+				}
+			}
+			else
+			{
+				first=false;
+				action=(int)arr[i];
+			}
+		}
+		
+		
+		double numOne=Double.parseDouble(String.join("",firstNumList));
+		double numTwo=Double.parseDouble(String.join("",secondNumList));
+		double res=0;
+		switch(action)
+		{
+			case 42://*
+				res=numOne*numTwo;
+				break;
+			case 43://+
+				res=numOne+numTwo;
+				break;
+			case 45://-
+				res=numOne-numTwo;
+				break;
+			case 46://.
+				break;
+			case 47:///
+				res=numOne/numTwo;
+				break;
+			case 99://c
+				res=Math.cos(numOne);
+				break;
+			case 97://a
+				res=Math.acos(numOne);
+				break;
+			case 115://s
+				res=Math.sin(numOne);
+				break;
+			case 98://b
+				res=Math.asin(numOne);
+				break;
+			case 116://t
+				res=Math.tan(numOne);
+				break;
+			case 100://d
+				res=Math.atan(numOne);
+				break;
+			case 94://^
+				res=Math.pow(numOne,numTwo);
+				break;
+		}
+		
+		return res;
+	}
 }
